fccc7d63e9dc3e77b7e0389d2267a394092afd83
Haru.syren <90908637+haruusyren@users.noreply.github.com>
2022-02-13 14:58:39 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,19 @@ module.exports = {
     name: 'ready',
     once: true,
     execute() {
-        console.log('Ready !')
-        Client.user.setPresence( { activities: [ { name: ` : Créé par 𝕯𝖊𝖛'𝕽𝖚#0992`, type: 'STREAMING', url: 'https://twitch.tv/syren_off/' } ], status: 'dnd' } ); },
+        console.log("Ready !")
+
+        Client.user.setPresence(
+            {
+                activities: [
+                    {
+                        name: ` : Créé par 𝕯𝖊𝖛'𝕽𝖚#0992`,
+                        type: 'STREAMING',
+                        url: 'https://twitch.tv/syren_off/'
+                    }
+                ],
+                status: 'dnd'
+            }
+        );
+    },
 }
